a2a59347cad64f25b107d8357e6f80202c500ad4
Memo <fretn655@gmail.com>
2026-04-20 10:45:19 -0600

    se cargan las importaciones necesarias

---
+++ Proyecto7TecMty.py
@@ -0,0 +1,9 @@
+# ------------- Importaciones
+
+import pandas as pd
+
+"""para el drive"""
+# from google.colab import drive
+# drive.mount("/content/gdrive")
+
+# ------------- Fin de las importaciones
