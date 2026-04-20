0244f2f5c449c0915168d78e4a15c43dc6238faa
Memo <fretn655@gmail.com>
2026-04-20 11:00:45 -0600

    se concatenan los 4 dfs en un solo df_full

--- Proyecto7TecMty.py
+++ Proyecto7TecMty.py
@@ -13,3 +13,6 @@ df_test = pd.read_csv(r"Test.csv")
 df_test2 = pd.read_csv(r"test2.csv")
 df_train2 = pd.read_csv(r"Train2.csv")
 df_train = pd.read_csv(r"Train.csv")
+
+"""2. Se concatenan los 4 df's"""
+df_full = pd.concat([df_test, df_test2, df_train2, df_train], ignore_index=True)
