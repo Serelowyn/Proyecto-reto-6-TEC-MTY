c2fe14fbfc79635439e60682aa1ffe4699cd2f66
Memo <fretn655@gmail.com>
2026-04-20 10:49:34 -0600

    se cargan los exceles

--- Proyecto7TecMty.py
+++ Proyecto7TecMty.py
@@ -7,3 +7,9 @@ import pandas as pd
 # drive.mount("/content/gdrive")
 
 # ------------- Fin de las importaciones
+
+"""1. Se cargan los archivos csv, se cambian datos en el drive para que funcione"""
+df_test = pd.read_csv(r"Test.csv")
+df_test2 = pd.read_csv(r"test2.csv")
+df_train2 = pd.read_csv(r"Train2.csv")
+df_train = pd.read_csv(r"Train.csv")
