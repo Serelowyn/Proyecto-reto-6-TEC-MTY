9be572c002f3aa4cafb57405c5cbc508289ae75d
Memo <fretn655@gmail.com>
2026-04-20 13:31:08 -0600

    se hacen cambios importantes. El mas importante fue la forma en la que se unen los 4 dfs en la linea 30 y 31.

--- Proyecto7TecMty.py
+++ Proyecto7TecMty.py
@@ -19,33 +19,31 @@ print(df_test2.columns)
 print(df_train2.columns)
 
 """aqui eliminamos los espacios de todas las colunmnas"""
-df_test2.columns = df_test2.columns.str.strip()
-df_train2.columns = df_train2.columns.str.strip()
+for df in [df_test, df_test2, df_train2, df_train]:
+    df.columns = df.columns.str.strip().str.lower().str.replace(" ", "_")
 
 """verificamos resultados"""
 print(df_test2.columns.tolist())
 print(df_train2.columns.tolist())
 
 """2. Se concatenan los 4 df's"""
-df_full = pd.concat([df_test, df_test2, df_train2, df_train], ignore_index=True)
+df_full = pd.concat([df_train, df_train2, df_test, df_test2], axis=0, ignore_index=True)
+df_full = df_full.groupby("employee_id").first().reset_index()
 
 """3. Aqui movemos las dos columnas que se indica en las instrucciones"""
-df_full["Computer_OS"] = df_full["Computer_OS"].fillna("no registrado")
-df_full["Mobile_OS"] = df_full["Mobile_OS"].fillna("no registrado")
+df_full["computer_os"] = df_full["computer_os"].fillna("no registrado")
+df_full["mobile_os"] = df_full["mobile_os"].fillna("no registrado")
 
 """4. calculamos los promedios de uso de cada sistema operativo"""
-porcentajes_os = df_full["Computer_OS"].value_counts(normalize=True) * 100
+porcentajes_os = df_full["computer_os"].value_counts(normalize=True) * 100
 print(porcentajes_os)
 """comprobamos que la suma de todo de 100"""
 print(porcentajes_os.sum())
 
 """calculamos los promedios de uso de sistemas operativos de celular"""
-porcentaje_cel = df_full["Mobile_OS"].value_counts(normalize=True) * 100
+porcentaje_cel = df_full["mobile_os"].value_counts(normalize=True) * 100
 print(porcentaje_cel)
 
 """5. sacamos la edad promedio de la gente que usa el OS"""
-promedio_edad_os = df_full.groupby("Computer_OS")["Age"].mean()
+promedio_edad_os = df_full.groupby("computer_os")["age"].mean()
 print(promedio_edad_os)
-df_full.columns.tolist()
-df_full.groupby("Computer_OS")["Age"].count()
-df_full.groupby("Mobile_OS")["Age"].count()
