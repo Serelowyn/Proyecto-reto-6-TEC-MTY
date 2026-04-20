194908c8cd377ca49ed75a997e30717ef49151f9
Memo <fretn655@gmail.com>
2026-04-20 12:08:20 -0600

    se hace la parte en la que rellenamos los NaN de ComputerOS y de MobileOS y adicional a esto hacemos una mini limpieza, empezando por quitar el espacio del inicio

--- Proyecto7TecMty.py
+++ Proyecto7TecMty.py
@@ -16,3 +16,11 @@ df_train = pd.read_csv(r"Train.csv")
 
 """2. Se concatenan los 4 df's"""
 df_full = pd.concat([df_test, df_test2, df_train2, df_train], ignore_index=True)
+
+"""3. Aqui movemos las dos columnas que se indica en las instrucciones"""
+df_full[" Computer_OS"] = df_full[" Computer_OS"].fillna("no registrado")
+df_full[" Mobile_OS"] = df_full[" Mobile_OS"].fillna("no registrado")
+
+"""en la parte de arriba se encontro que los nombres Computer_OS y Mobile_OS tenian un espacio en la parte del inicio adicional, entonces aqui lo elimino"""
+df_full.columns = df_full.columns.str.strip().str.replace(" ", "")
+print(df_full.columns.tolist())#verificar que los cambios hayan sido exitosos
